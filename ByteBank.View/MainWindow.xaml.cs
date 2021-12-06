284ee825edd51f2f54958f49791c6270ba5a7c08
AndersonMiyake <37424749+AndersonMiyake@users.noreply.github.com>
2021-12-06 20:25:49 -0300

    Add files via upload

---
+++ ByteBank.View/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using ByteBank.Core.Model;
+using ByteBank.Core.Repository;
+using ByteBank.Core.Service;
+using ByteBank.View.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ByteBank.View
+{
+    public partial class MainWindow : Window
+    {
+        private readonly ContaClienteRepository r_Repositorio;
+        private readonly ContaClienteService r_Servico;
+        private CancellationTokenSource _cts;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            r_Repositorio = new ContaClienteRepository();
+            r_Servico = new ContaClienteService();
+        }
+
+        private async void BtnProcessar_Click(object sender, RoutedEventArgs e)
+        {
+            //Obtendo o taskScheduler da thread principal
+            //var taskSchedulerUI = TaskScheduler.FromCurrentSynchronizationContext();
+
+            BtnProcessar.IsEnabled = false;
+
+            _cts = new CancellationTokenSource();
+
+            var contas = r_Repositorio.GetContaClientes();
+
+            PgsProgresso.Maximum = contas.Count();
+
+            LimparView();
+
+            var inicio = DateTime.Now;
+
+            BtnCancelar.IsEnabled = true;
+
+            var progress = new Progress<String>(str =>
+            PgsProgresso.Value++);
+            //var byteBankProgress = new ByteBankProgress<string>(str =>
+            //    PgsProgresso.Value++);
+
+            try
+            {
+                var resultado = await ConsolidarContas(contas, progress, _cts.Token);
+
+                var fim = DateTime.Now;
+                AtualizarView(resultado, fim - inicio);
+            }
+            catch (OperationCanceledException)
+            {
+                TxtTempo.Text = "Operação cancelada pelo usuário.";
+            }
+            finally
+            {
+                BtnProcessar.IsEnabled = true;
+                BtnCancelar.IsEnabled = false;
+            }
+        }
+
+        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            BtnCancelar.IsEnabled = false;
+            _cts.Cancel();
+        }
+
+        private async Task<string[]> ConsolidarContas(IEnumerable<ContaCliente> contas, IProgress<string> reportadorDeProgresso, CancellationToken ct)
+        {
+            var tasks = contas.Select(conta =>
+                Task.Factory.StartNew(() =>
+                {
+                    ct.ThrowIfCancellationRequested();
+
+                    var resultadoConsolidacao = r_Servico.ConsolidarMovimentacao(conta, ct);
+
+                    reportadorDeProgresso.Report(resultadoConsolidacao);
+
+                    ct.ThrowIfCancellationRequested();
+
+                    return resultadoConsolidacao;
+                }, ct)
+            );
+
+            return await Task.WhenAll(tasks);
+        }
+
+        private void LimparView()
+        {
+            LstResultados.ItemsSource = null;
+            TxtTempo.Text = null;
+            PgsProgresso.Value = 0;
+        }
+
+        private void AtualizarView(IEnumerable<String> result, TimeSpan elapsedTime)
+        {
+            var tempoDecorrido = $"{ elapsedTime.Seconds }.{ elapsedTime.Milliseconds} segundos!";
+            var mensagem = $"Processamento de {result.Count()} clientes em {tempoDecorrido}";
+
+            LstResultados.ItemsSource = result;
+            TxtTempo.Text = mensagem;
+        }
+
+    }
+}
